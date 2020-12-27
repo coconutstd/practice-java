f8357bd604cca2991280352a6f2abb9fac81b465
junui <wnsspdl@naver.com>
2020-12-28 04:07:03 +0900

    Korean constructor

--- src/com/company/coconut/CarExample.java
+++
@@ -1,7 +0,0 @@
-package com.company.coconut;
-
-public class CarExample {
-    public static void main(String[] args){
-        System.out.println("hello car");
-    }
-}
--- src/com/company/coconut/Main.java
+++ src/com/company/coconut/Main.java
@@ -1,7 +1,10 @@
 package com.company.coconut;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello java");
+        Korean k1 = new Korean("박자바", "011255-1234567");
+        System.out.println("k1.name: " + k1.name);
     }
 }
