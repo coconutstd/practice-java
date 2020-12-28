6d8b56b69ba9f4f9615623ad138eaa1d315c6a21
junui <wnsspdl@naver.com>
2020-12-28 17:48:15 +0900

    자바의 배열

--- src/com/company/coconut/Main.java
+++ src/com/company/coconut/Main.java
@@ -2,6 +2,14 @@ package com.company.coconut;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("hello java");
+        int[] scores = { 95, 71, 84, 93, 87 };
+
+        int sum = 0 ;
+        for (int score : scores) {
+            sum += score;
+        }
+        System.out.println("점수 총합 = " + sum);
+        double avg = (double) sum / scores.length;
+        System.out.println("점수 평균 = " + avg);
     }
 }
