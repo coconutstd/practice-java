5623a4bb7d06b86393458f009995b87d7c8cc4fc
junui <wnsspdl@naver.com>
2020-12-28 21:31:40 +0900

    자바 객체지향1

--- src/com/company/coconut/Main.java
+++ src/com/company/coconut/Main.java
@@ -6,11 +6,6 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Map<Student, Integer> map = new HashMap<>();
 
-        map.put(new Student(1, "홍길동"), 95);
-        map.put(new Student(1, "홍길동"), 95);
-
-        System.out.println("총 Entry 수: " + map.size());
     }
 }
---
+++ src/com/company/objects/Singleton.java
@@ -0,0 +1,11 @@
+package com.company.objects;
+
+public class Singleton {
+    private static Singleton singleton = new Singleton();
+
+    private Singleton() {}
+
+    static Singleton getInstance(){
+        return singleton;
+    }
+}
